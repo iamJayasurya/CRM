935fc3232471b58cd2aa846fe497838a8f242d5a
iamJayasurya <surya.appac@gmail.com>
2025-08-22 17:45:38 +0530

    day -01 Setup complete

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,31 +1,42 @@
 "use client";
 import { useState } from "react";
-
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 export default function Home() {
+  const [formData , setFormData] =useState([])
   const [data, setData] = useState({
     name: "",
     date: "",
     password: "",
   });
   console.log(data);
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setData((prevData) => ({
       ...prevData,
-      [name]: value,
-    }))
-
-  }
+      [name]:value,
+      }))
+}
   
-  const HandleSubmit = () => {
-    alert(JSON.stringify(data,null,2))
-                   
+  const HandleSubmit = (e) => {
+    e.preventDefault(); 
+    setFormData((prev)=>[...prev ,data])
+    // alert(JSON.stringify(data,null,2))
+      setData   ({    name: "",
+    date: "",
+    password: "",})          
   }
   return (
-    <section className="flex justify-center items-center">
+    <section className="flex justify-center items-center h-[100%] w-full">
       <div className="p-[30px]  w-[600px] h-[auto]">
-        <form action="post" className="grid grid-cols-2 gap-[20px]  text-[#000]" onSubmit={HandleSubmit}>
+        <form  className="grid grid-cols-2 gap-[20px]  text-[#000]" >
           <input
             className="text-[#000]  border-1"
             type="text"
@@ -50,8 +61,28 @@ export default function Home() {
             id=""
                  onChange={handleChange}
           />
-          <button type="submit">Submit</button>
+          <button onClick={HandleSubmit}>Submit</button>
         </form>
+        <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+          <TableBody>
+            {Array.isArray(formData) && formData.map((data, index) => (
+  <TableRow key={index}>
+    <TableCell className="font-medium">{data.name}</TableCell>
+    <TableCell>{data.date}</TableCell>
+    <TableCell>{data.password}</TableCell>
+  </TableRow>
+))}
+  </TableBody>
+</Table>
       </div>
     </section>
   );
